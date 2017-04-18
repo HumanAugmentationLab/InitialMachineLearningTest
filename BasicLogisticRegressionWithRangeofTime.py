55533179146a6e8bb6b3d1c82aaf747f93956d4c
William Derksen <william.derksen@students.olin.edu>
2017-04-18 17:08:51 -0400

    Working, but no Cross Validation

--- BasicLogisticRegressionWithRangeofTime.py
+++ BasicLogisticRegressionWithRangeofTime.py
@@ -2,38 +2,44 @@ import numpy as np
 import theano
 import theano.tensor as T
 import pandas as pd
+import random
+from collections import Counter
+from matplotlib import pyplot as plt
+import sys
+import time
+
+tpre = time.time()
 rng = np.random
 
 #Retrieve dataset
 dataname = "BCICIV_calib_ds1a_cnt.txt"
 Srate = 100
-start_ofst = int(.5*Srate)
-end_ofst = int(3*Srate)
-resolution = int(Srate*.1) #Lower number is higher resolution
+start_ofst = int(0*Srate)
+end_ofst = int(5*Srate)
+resolution = int(Srate*.2) #Lower number is higher resolution
 frontstrip = int(Srate*14) #number is seconds to cut from the beginning
-endstrip = int(Srate*4) #number is seconds to cut from end
-file_reso = int(Srate*.05) #naumber is how ofter (in seconds) data is taken (this lowers samplerate or resolution)
+endstrip = int(Srate*5) #number is seconds to cut from end
+file_reso = int(Srate*.4) #naumber is how ofter (in seconds) data is taken (this lowers samplerate or resolution)
 markerSize = int(Srate*.5)
+train_set_size = .6 #percent of data to use for training vs percent to use for testing
 #Start to get memmory errors if the two resolution numbers multiply to be less than .005
+#(This is because the min they can be is .01, .005 choose the same data point for each part of the for loop these numbers work in, and thus create an infinite loop.)
 
 markername = "BCICIV_calib_ds1a_mrk.txt"
 eegnames = [str(i) for i in range(59)]
-#print(eegnames)
-
 txtdata = []
 df = pd.read_csv(dataname, sep='\t', header = None, names=eegnames)
 mf = pd.read_csv(markername, sep='\t', header = None, names = ['timestamp', 'class'])
 
-#print(df.loc[3:7])
-#print(mf.loc[:,'timestamp'])
-reg = .0001 #regularization parameter
-learnrate = .001 #learning rate
-training_steps = 50 #training steps
+reg = 10000 #regularization parameter
+acc = []
+deltashrink = .875
+deltagrow = 1.001
+training_steps = 750 #training steps
 num_classes = 3 #one for left, one for foot, and one for neither
 
 data = np.array(df)
 markers = np.array(mf)
-
 oldN, oldfeats = data.shape
 
 #### Setting time points in the range of "markersize" after the marker timepoint to the marker class ####
@@ -44,57 +50,102 @@ for mark in markers:
 
 Marks = Marks[frontstrip:len(Marks)-endstrip]
 
-#### DownSampling Entire Data ####
+#### DownSampling Entire Dataset ####
+interdata = [] # data before shuffling and splitting into datasets
+intermarks = [] # marks corresponding with above data.
+timepoint = 0
+while timepoint < len(Marks):
+    if timepoint % file_reso == 0 or Marks[timepoint] != 1:
+        interdata.append(np.reshape(data[timepoint+start_ofst:timepoint+end_ofst:resolution],-1)) # Downsampling the feature vector
+        intermarks.append(Marks[timepoint])
+    timepoint+=1
+interN = len(interdata)
+randindex = [i for i in range(interN)] #create index list
+rng.shuffle(randindex) #randomize list
 finaldata = []
 finalmarks = []
-i = 0
-for timepoint in Marks:
-    if i % file_reso == 0 or Marks[i] != 1:
-        finaldata.append(np.reshape(data[timepoint+start_ofst:timepoint+end_ofst:resolution],-1)) # Downsampling the feature vector
-        finalmarks.append(Marks[i])
-    i+=1
+testdata = []
+testmarks = []
+train_actual_size = interN*train_set_size
+for i in range(interN): #pick "train_set_size" of the data and markers and put it into the training set
+    if i < train_actual_size:
+        finaldata.append(interdata[randindex[i]])
+        finalmarks.append(intermarks[randindex[i]])
+    else:
+        testdata.append(interdata[randindex[i]])
+        testmarks.append(intermarks[randindex[i]])
 finaldata = np.array(finaldata)
 finalmarks = np.array(finalmarks, dtype = 'int32')
+testdata = np.array(testdata)
+testmarks = np.array(testmarks, dtype = 'int32')
+print('trainsizes')
 print(finaldata.shape)
 print(finalmarks.shape)
-#print(len(timedata))
+print('\ntestsizes')
+print(testdata.shape)
+print(testmarks.shape)
 
+
+print(finalmarks)
 N, feats = finaldata.shape
+
+initw = rng.randn(feats,num_classes)
+initb = rng.randn(num_classes)
+
+ttrain = time.time()
+learnrate = theano.shared(.0001, name="learnrate") #learning rate
+
 
 #Declare Theano Symbolics
 x = T.dmatrix('x')
 y = T.ivector('y')
+z = T.dmatrix('z')
 
 #Initialize weight vector randomly (use shared so they stay the same after the generation)
-w = theano.shared(rng.randn(feats,num_classes), name="w")
+w = theano.shared(initw, name="w")
 
 #Initialize bias vector b
-b = theano.shared(.01, name="b")
+b = theano.shared(initb, name="b")
+
 
 #construct formulas with Symbolics
-#p_1 = 1/(1+T.exp(-T.dot(x,w)-b)) #Calculate accuracy prediction
-#prediction = p_1 > .5
 sigma = T.nnet.softmax(T.dot(x,w)+b) #SoftMax of the classes
 prediction = T.argmax(sigma, axis=1) #The most probable class.
-#xent = -y * T.log(p_1) - (1-y) * T.log(1-p_1)
-xent = -T.mean(T.log(sigma)[T.arange(y.shape[0]),y]) #Cross Entropy Function
+
+### Z Fuction ###
+zsigma = T.nnet.softmax(T.dot(z,w)+b) #SoftMax of the classes
+zprediction = T.argmax(zsigma, axis=1)
+
+xent = -T.mean(T.log(sigma)[T.arange(y.shape[0]),y]) #Cross Entropy Function For Multi Class Optimization
 cost = xent.mean() + reg * (w**2).sum() #cost function with regularization
 gw, gb = T.grad(cost, [w,b]) #Gradient computation
 
 #Instantiate formulas with theano.function
 train = theano.function(inputs = [x,y],
                         outputs = [prediction, cost],
-                        updates = ((w, w - learnrate*gw), (b, b - learnrate*gb)))
-predict = theano.function(inputs = [x], outputs = prediction)
+                        updates = ((w, w - learnrate*gw), (b, b - learnrate*gb), (learnrate, learnrate*deltagrow)))
+predict = theano.function(inputs = [z], outputs = zprediction)
 
 #Train
+old_err = sys.float_info.max
 for i in range(training_steps):
-    print('trainstep' + str(i))
+    print('\ntrainstep' + str(i))
     pred, err = train(finaldata,finalmarks)
+    if err > old_err*1.0001:
+        learnrate.set_value(learnrate.get_value()*deltashrink)
+    old_err = err
     print(err)
+    print(learnrate.get_value())
 
-finalpred = predict(finaldata)
-answers = np.array(finalmarks)
+ttest = time.time()
+finalpred = predict(testdata)
+answers = np.array(testmarks)
+print(finalpred)
+print(answers)
+c = Counter(finalpred)
+d = Counter(answers)
+print(c)
+print(d)
 finalpred2 = finalpred*5
 PosNeg = finalpred2 - answers
 
@@ -115,27 +166,78 @@ OP = PosNegCount.get(3)[1]
 NO = PosNegCount.get(-1)[1]
 NP = PosNegCount.get(-2)[1]
 
+Npred = len(answers)
+
 print(PosNegCount)
-#Print Results
 print("Final model:")
-print(w.get_value())
-print(b.get_value())
 print("True Positive Rate:")
 print(PP/(PP+OP+NP))
 print("True Neutral Rate:")
 print(OO/(PO+OO+NO))
 print("True Negative Rate:")
 print(NN/(PN+ON+NN))
-accuracyList = [1 if finalpred[i] == answers[i] else 0 for i in range(N)]
-#for i in range(N):
-#    accuracyList.append(finalpred[0] == answers[0])
+accuracyList = [1 if finalpred[i] == answers[i] else 0 for i in range(Npred)]
 right = sum(accuracyList)
-print(N-right)
-print(right/N)
-print(N)
-#print("False Positive Rate:")
-#print(FP/(FP+TN))
-#print("False Neutral Rate:")
-#print()
-#print("False Negative Rate:")
-#print()
+acc.append(right/Npred)
+print(acc)
+
+
+tfinish = time.time()
+print("Preprocessing Time: %f" %(ttrain-tpre))
+print("Training Time: %f" %(ttest-ttrain))
+print("Testing Time: %f" %(tfinish-ttest))
+
+
+## TODO
+"""
+Add cross-validation SciKit or manual (Try SciKit first)
+Understand why values are only ever perfect or completely off
+Determine whether it is correctly training and testing (Appears to be somewhat correct?, maybe fake data is just very good fake data, try with non fake data)
+Add Backtracking Search Algorithm
+
+########### Long Stretch: Network Versions ##############
+
+
+#### Done:
+
+Networks:
+LDA
+
+Functions:
+Sigmoid
+
+Descent Algorithms:
+Gradient Descent (Backpropagation Algorithm)
+
+Parameterization:
+Flexible Learn Rate with Growth and Shrink
+
+Other:
+
+
+
+
+#### TBD (To Be Done):
+
+Networks:
+MLP
+Convolutional**
+Fuzzy?
+RNN/LSTM*
+RBF or Radial Basis Function*
+
+Functions:
+RELU?
+SVM**
+
+Descent Algorithms:
+LBFGS
+
+Parameterization:
+Cross Validation
+Backtracking Search Algorithm
+
+Other:
+CSP*
+
+"""
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # InitialMachineLearningTest
 Initial test to do machine learning on BCI data using python.
+
+
+## Singular Perceptron Neural Network (or Linear Discriminant Analysis ??)
+
+This setup used a single perceptron, or one network of weights leading from inputs straight to outputs with no hidden layer.  This allows for relatively quick training but greatly limits the complexity of the solutions the network generates.
+
+In order to get the data into a format that could be used in an LDA but still classify data over a measure of time, The information from the 59 electrodes at each time point was added to timepoints after the designated time as well to get a range of information from the EEG at the current timestep and future timesteps.  The reason we look at the future data in order to categorize whether the person is thinking of left foot or right hand, is because the data specifies when the person was told to start thinking of which class, rather than when the person was actually thinking of said class.  This way we can train the network to categorize what the person was told to do based on the reaction they have to said instruction in the proceeding seconds.
+
+###First Dataset Results (ds1b [developed first on the second dataset.])
+Accuracy: ~70-75% with 750 trials (~75-80% with 7500 trials)
+Preprocessing Time: 1.669661s
+Training Time: 64.005466s
+Testing Time: 0.046811s
+
+In order to maximize the result of this data a lot of messing with parameters was done and without cross validation.  This means that the data could very well be skewed to only working well on this data.  A good way to test the validity of these parameters for the training is to simply use the same parameters on another set of data and see how different the accuracy is.
+
+###Second Dataset Results (ds1a [tested on the first dataset])
+Accuracy: ~50%
+Preprocessing Time: 1.881018
+Training Time: 65.029943
+Testing Time: 0.042454
+
+This shows that the lack of cross validation caused a significant amount of the increased accuracy in the First Dataset's Results
